1e2971750b51447b61f721cd5d83de32c042515a
sethsaperstein <sethsaper@gmail.com>
2019-08-07 10:39:58 -0400

    Initial Commit

---
+++ visitation-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  visitation-app
+//
+//  Created by Seth Saperstein on 8/7/19.
+//  Copyright © 2019 Seth Saperstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
